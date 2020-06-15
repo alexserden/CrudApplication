db2935790ab88deb5e901fe98ef08adfa7cb1903
aleksandr.denisenko <Geiythv48!q561&>
2020-06-15 20:32:19 +0300

    commit - 10

--- src/main/java/com/alexserden/task/repository/CustomerRepository.java
+++ src/main/java/com/alexserden/task/repository/CustomerRepository.java
@@ -5,7 +5,5 @@ import com.alexserden.task.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
     List<Customer> findByLastName(String lastname);
-    
 }
